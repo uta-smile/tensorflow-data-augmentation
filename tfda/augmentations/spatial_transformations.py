b8d781b85c615490f8c3b005ca16496355a238b7
Nasy <nasyxx+git@gmail.com>
2021-12-24 15:31:38 -0600

    Fixed

--- tfda/augmentations/spatial_transformations.py
+++ tfda/augmentations/spatial_transformations.py
@@ -306,7 +306,7 @@ def augment_spatial(
     cond_to_loop = lambda sample_id, patch_size, data, seg, data_result, seg_result: tf.less(
         sample_id, sample_num
     )
-    dim = tf.shape(patch_size, dtype=tf.int64)[0]
+    dim = tf.cast(tf.shape(patch_size)[0], tf.int64)
     seg_result = None
     if seg is not None:
         seg_result = tf.cond(
