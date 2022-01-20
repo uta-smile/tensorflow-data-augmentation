ad4113121771a1d8406bd7483a28aeee54e12882
Nasy <nasyxx+git@gmail.com>
2022-01-20 11:29:09 -0600

    Add init

---
+++ tfda/transforms/pyramid_augmentations.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+r"""
+Python ♡ Nasy.
+
+    |             *         *
+    |                  .                .
+    |           .                              登
+    |     *                      ,
+    |                   .                      至
+    |
+    |                               *          恖
+    |          |\___/|
+    |          )    -(             .           聖 ·
+    |         =\ -   /=
+    |           )===(       *
+    |          /   - \
+    |          |-    |
+    |         /   -   \     0.|.0
+    |  NASY___\__( (__/_____(\=/)__+1s____________
+    |  ______|____) )______|______|______|______|_
+    |  ___|______( (____|______|______|______|____
+    |  ______|____\_|______|______|______|______|_
+    |  ___|______|______|______|______|______|____
+    |  ______|______|______|______|______|______|_
+    |  ___|______|______|______|______|______|____
+
+author   : Nasy https://nasy.moe
+date     : Jan 20, 2022
+email    : Nasy <nasyxx+python@gmail.com>
+           WlZhong <wxz9204@mavs.uta.edu>
+filename : pyramid_augmentations.py
+project  : transforms
+license  : GPL-3.0+
+
+https://github.com/MIC-DKFZ/nnUNet/
+  nnunet/training/data_augmentation/pyramid_augmentations.py
+"""
